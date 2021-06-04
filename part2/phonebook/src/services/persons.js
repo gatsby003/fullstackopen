02eb54c7ec40ebbbfc5cfab6adac68ea15bb3c28
gatsby003 <ganeshfutane3@gmail.com>
2021-06-04 20:26:55 +0530

    fixed delete

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://boiling-springs-96480.herokuapp.com/api/persons/'
+const baseUrl = 'api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
